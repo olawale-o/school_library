1f0a62b4ed20f233b6e651235b8f76ada9f128b6
olawale-o <omoogunolawale@gmail.com>
2021-10-05 05:54:40 +0100

    Define list_all_books method

--- main.rb
+++ main.rb
@@ -1,4 +1,41 @@
+require_relative 'book'
+require_relative 'person'
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'rental'
+
+class Application
+  def initialize
+    @books = []
+    @people = []
+  end
+
+  def show_welcome_screen
+    puts "Welcome to school Library App!\n\n"
+    puts 'Please choose an option by entering a number: '
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+
+  def user_input
+    gets.chomp
+  end
+
+  def list_all_books
+    @books.map { |book| puts "Title: #{book.title}, Author: #{book.author}" }
+  end
+
+
+end
+
 def main()
+  app = Application.new
+  app.run
 end
 
 main
