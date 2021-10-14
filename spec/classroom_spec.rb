b691b2a8e5a0c4210e49069739120d5f0a9e149b
olawale-o <omoogunolawale@gmail.com>
2021-10-14 08:42:25 +0100

    Add Classroom test

---
+++ spec/classroom_spec.rb
@@ -0,0 +1,45 @@
+require_relative 'spec_helper'
+
+describe Classroom do
+  describe '#new' do
+    let(:classroom) { Classroom.new({ label: 'Science' }) }
+    context 'when classroom is instantiated' do
+      it 'should return an instance of Classroom' do
+        expect(classroom).to be_an_instance_of Classroom
+      end
+      it 'should respond to students' do
+        expect(classroom).to respond_to(:students)
+      end
+      it 'should respond to label' do
+        expect(classroom).to respond_to(:students)
+      end
+      it 'should return label to be Science' do
+        expect(classroom.label).to eq 'Science'
+      end
+      it 'should have students attribute' do
+        expect(classroom).to have_attributes(students: [])
+      end
+    end
+  end
+
+  describe '#add_student' do
+    classroom2 = Classroom.new({ label: 'Art' })
+    student = Student.new({ name: 'Elijah', age: 23, classroom: classroom2 })
+    student2 = Student.new({ name: 'Ashkae', age: 22, classroom: classroom2 })
+    classroom2.add_student student
+    classroom2.add_student student2
+    context 'classroom' do
+      it 'should return student' do
+        expect(classroom2.students).to include(student)
+      end
+      it 'should have more than 1 student' do
+        expect(classroom2.students.size).to be > 1
+      end
+    end
+    context 'student' do
+      it 'should belong to classroom2 ' do
+        expect(student.classroom).to eq classroom2
+      end
+    end
+  end
+end
